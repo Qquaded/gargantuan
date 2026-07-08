a3487e7b5ad615c9bc7f0f7865bdf0f8967c00bc
znotfireman <znotfireman@gmail.com>
2026-07-09 03:01:21 +0700

    feat: fps counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include "raylib.h"
+#include <string>
+using namespace std;
+
 int main() {
+	InitWindow(320, 240, "Zero");
+	SetTargetFPS(120);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+			ClearBackground(BLACK);
+			string fpsLabel = "FPS: " + to_string(GetFPS());
+			DrawText(fpsLabel.c_str(), 0, 0, 20, WHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
 	return 0;
 }
